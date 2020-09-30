9a5e9d13fdce66c5424d148f85ca3e8019c5e023
arund <ardogra1999@gmail.com>
2020-09-30 17:26:18 +0530

    Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return(MaterialApp(
-      title: 'Dummy Data',
+    return MaterialApp(
+      title: 'ddsf',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Dummy Data'),
+          title: Text('asdas'),
         ),
         body: Container(
-          child: Center(
-            child: Text('Dummy Data'),
-          ),
+          child: Text('Demo'),
         ),
       ),
-    ));
+    );
   }
 }
